77c6478da8b5bac4c4ce7506efb797490186f42e
Philip <philip12m2003@gmail.com>
2024-09-23 01:20:34 +0200

    init

---
+++ main.js
@@ -0,0 +1,40 @@
+import './style.css'
+import * as THREE from 'three';
+
+const width = globalThis.innerWidth;
+const height = globalThis.innerHeight;
+
+const camera = new THREE.PerspectiveCamera( 70, width / height, 0.01, 10 );
+camera.position.z = 1;
+
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+const material = new THREE.MeshNormalMaterial();
+
+const mesh = new THREE.Mesh( geometry, material );
+scene.add( mesh );
+
+const canvas = document.querySelector('#back-ground');
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true,
+});
+renderer.setPixelRatio(globalThis.devicePixelRatio);
+renderer.setSize(globalThis.innerWidth, globalThis.innerHeight);
+
+function render(time) {
+  time *= 0.001;  // convert time to seconds
+ 
+  mesh.rotation.x = time;
+  mesh.rotation.y = time;
+
+  renderer.setSize(globalThis.innerWidth, globalThis.innerHeight);
+  camera.aspect = canvas.clientWidth / canvas.clientHeight;
+  camera.updateProjectionMatrix();
+ 
+  renderer.render(scene, camera);
+ 
+  requestAnimationFrame(render);
+}
+requestAnimationFrame(render);
